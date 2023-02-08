7aab0d0d94d31c9e5d37dcc1582b1a73ffd2eb22
Imran Badshah <imrnbdshiscool@yahoo.co.in>
2023-02-08 03:08:31 +0100

    Grouped

--- extract_data.py
+++ extract_data.py
@@ -1,6 +1,7 @@
 import pydicom as dicom, argparse
 import os
 import logger
+from tabulate import tabulate
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -12,6 +13,8 @@ def parse_args():
                         required=False, help="include this for specifically extracting Study Instance UID Attribute")
     parser.add_argument("-s", "--SERIES_INSTANCE_UID", metavar="", action=argparse.BooleanOptionalAction,
                         required=False, help="include this for specifically extracting Series Instance UID Attribute")
+    parser.add_argument("-g", "--GROUP", metavar="", action=argparse.BooleanOptionalAction,
+                        required=False, help="Group by Study Instance UID Attribute")
 
     args = parser.parse_args()
     for arg in vars(args):
@@ -43,20 +46,51 @@ def extract_data(args):
     input_path = args.INPUT_PATH
     specific_extracts = construct_specific_extracts(args)
     if (os.path.isdir(input_path)):
-        # images_path = os.listdir(input_path)
-        # for n, image in enumerate(images_path):
-        #     print_data(os.path.join(input_path, image), specific_extracts)
         for root, _dirs, files in os.walk(input_path):
             for name in files:
                 print_data(os.path.join(root, name), specific_extracts)
     else:
         print_data(input_path, specific_extracts)        
-       
+
+def update_group(table_dict, target_path):
+    ds = dicom.dcmread(target_path)
+    try:
+        table_dict[ds[0x0020, 0x000D][:]][0] += 1
+        table_dict[ds[0x0020, 0x000D][:]][1].extend([target_path])
+    except:
+        table_dict[ds[0x0020, 0x000D][:]] = [1, [target_path]]
+    return table_dict
+     
+"""
+table:
+study-uuid  |   occurances  |   file_path
+"""
+def group_data(args):
+    input_path = args.INPUT_PATH
+    
+    table_dict = {}
+    table = []
+    headers = ["study-uuid", "occurances", "file_path"]
+
+    if (os.path.isdir(input_path)):
+        for root, _dirs, files in os.walk(input_path):
+            for name in files:
+                target_path = os.path.join(root, name)
+                table_dict = update_group(table_dict, target_path)
+    else:
+        table_dict = update_group(table_dict, input_path)
+    
+    print("table_dict: ", table_dict)
+    table = [ [k, table_dict.get(k)[0], '\n'.join(table_dict.get(k)[1])] for k in table_dict.keys() ]
+    print(tabulate(table, headers, tablefmt="simple_grid"))
 
 def main():
     args = parse_args()
     try:
-        extract_data(args)
+        if (args.GROUP):
+            group_data(args)
+        else:
+            extract_data(args)
     except Exception as e:
         logger.error("\nError extracting data: " + str(e))
     else:    
